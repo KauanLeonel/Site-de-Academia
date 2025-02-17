ea9d903f61bc9b4b6dedb596da2a79beaa493e34
Kauan Leonel <kauan.br.char@gmail.com>
2025-02-17 18:08:21 -0300

    Terminando o tutorial do vídeo com alguns erros a serem corrigidos

--- main.js
+++ main.js
@@ -5,3 +5,67 @@ menu.onclick = () =>{
     menu.classList.toggle('fa-times')
     navbar.classList.togglee('active')
 }
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-time')
+    menu.classList.remove('active')
+}
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    centeredSlides: true,
+    pagination: {  // Corrigido!
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        600:{
+            slidesPerView:2,
+        },
+    },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        991:{
+            slidesPerView:3,
+        },
+    },
+});
